86d2b5100a936798b6f16be1c43a5a8d53614040
karthikcng <karthikcng1995@gmail.com>
2023-06-28 13:32:57 +0530

    editing patient module

--- src/Modules/Patient/Patient.js
+++ src/Modules/Patient/Patient.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Patient1 } from "../../NavBar/Patient1";
+import "bootswatch/dist/spacelab/bootstrap.min.css";
 
 export const Patient = () => {
   return (
-    <div>Patient
-      
-      
-   
-</div>
-  )
-}
+    <div>
+      <Patient1 />
+      <div className="mt-5">
+        <div className="container">
+          <div className="add_btn mt-2 mb-2">
+            <table class="table">
+              <thead>
+                <tr className="table-dark">
+                  <th scope="col">Sl No</th>
+                  <th scope="col">Patient No</th>
+                  <th scope="col"> Patient Name</th>
+                  <th scope="col">Phone No</th>
+                  <th scope="col">Registration</th>
+                  <th scope="col">Action Buttons</th>
+                </tr>
+              </thead>
+            </table>
+            <tbody></tbody>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
