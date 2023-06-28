1bce9b438ea0b320bf8aaf66029c34b255bfa5a6
karthikcng <karthikcng1995@gmail.com>
2023-06-28 13:55:18 +0530

    push

--- src/NavBar/Patient1.js
+++ src/NavBar/Patient1.js
@@ -5,20 +5,6 @@ export const Patient1 = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-light" data-bs-theme="light">
         <div className="container-fluid">
-          {/* <a classNameName="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            classNameName="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarColor03"
-            aria-controls="navbarColor03"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span classNameName="navbar-toggler-icon"></span>
-          </button> */}
           <div className="collapse navbar-collapse" id="navbarColor03">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
